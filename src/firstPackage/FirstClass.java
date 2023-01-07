79b9f1cced4914e5c6b4ef1446156b6ae86d3f7b
baltugba <114306552+baltugba@users.noreply.github.com>
2023-01-07 14:33:04 -0500

    Update FirstClass.java

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -2,6 +2,7 @@ package firstPackage;
 
 public class FirstClass {
     public static void main(String[] args) {
+        //Some changes are made
         System.out.println("First class");
     }
 }
